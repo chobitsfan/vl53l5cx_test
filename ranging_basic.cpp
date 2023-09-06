e95303ef7beff615eabcd8bf8cc3e8ab2d7e1e29
chobits tai <chobits@itri.org.tw>
2023-09-06 16:22:29 +0800

    4x4

--- ranging_basic.cpp
+++ ranging_basic.cpp
@@ -85,57 +85,57 @@
 #include <geometry_msgs/PointStamped.h>
 #include "vl53l5cx_api.h"
 
-const double VL53L5_Zone_Pitch8x8[64] = {
-	59.00,64.00,67.50,70.00,70.00,67.50,64.00,59.00,
-	64.00,70.00,72.90,74.90,74.90,72.90,70.00,64.00,
-	67.50,72.90,77.40,80.50,80.50,77.40,72.90,67.50,
-	70.00,74.90,80.50,85.75,85.75,80.50,74.90,70.00,
-	70.00,74.90,80.50,85.75,85.75,80.50,74.90,70.00,
-	67.50,72.90,77.40,80.50,80.50,77.40,72.90,67.50,
-	64.00,70.00,72.90,74.90,74.90,72.90,70.00,64.00,
-	59.00,64.00,67.50,70.00,70.00,67.50,64.00,59.00
+#define TAN_B (sqrt(0.75*0.75+0.75*0.75)/tan(67.5*M_PI/180.0))
+#define TAN_A (sqrt(0.75*0.75+0.25*0.25)/tan(67.5*M_PI/180.0))
+#define TAN_C (sqrt(0.25*0.25+0.25*0.25)/tan(67.5*M_PI/180.0))
+
+const double TanOfPitch[16] = {
+    TAN_B, TAN_A, TAN_A, TAN_B,
+    TAN_A, TAN_C, TAN_C, TAN_A,
+    TAN_A, TAN_C, TAN_C, TAN_A,
+    TAN_B, TAN_A, TAN_A, TAN_B
 };
-const double VL53L5_Zone_Yaw8x8[64] = {
-	135.00,125.40,113.20, 98.13, 81.87, 66.80, 54.60, 45.00,
-	144.60,135.00,120.96,101.31, 78.69, 59.04, 45.00, 35.40,
-	156.80,149.04,135.00,108.45, 71.55, 45.00, 30.96, 23.20,
-	171.87,168.69,161.55,135.00, 45.00, 18.45, 11.31,  8.13,
-	188.13,191.31,198.45,225.00,315.00,341.55,348.69,351.87,
-	203.20,210.96,225.00,251.55,288.45,315.00,329.04,336.80,
-	203.20,225.00,239.04,258.69,281.31,300.96,315.00,324.60,
-    225.00,234.60,246.80,261.87,278.13,293.20,305.40,315.00
+
+#define SIN_45 (1.0/sqrt(2))
+#define SIN_A (0.75/sqrt(0.75*0.75+0.25*0.25))
+#define SIN_B (0.25/sqrt(0.75*0.75+0.25*0.25))
+
+const double SinOfYaw[16] = {
+    SIN_45, SIN_A, SIN_A, SIN_45,
+    SIN_B, SIN_45, SIN_45, SIN_B,
+    -SIN_B, -SIN_45, -SIN_45, -SIN_B,
+    -SIN_45, -SIN_A, -SIN_A, -SIN_45
 };
-double SinOfPitch[64], CosOfPitch[64], SinOfYaw[64], CosOfYaw[64];
+
+#define COS_45 (1.0/sqrt(2))
+#define COS_A (0.25/sqrt(0.75*0.75+0.25*0.25))
+#define COS_B (0.75/sqrt(0.75*0.75+0.25*0.25))
+
+const double CosOfYaw[16] = {
+    -COS_45, -COS_A, COS_A, COS_45,
+    -COS_B, -COS_45, COS_45, COS_B,
+    -COS_B, -COS_45, COS_45, COS_B,
+    -COS_45, -COS_A, COS_A, COS_45
+};
 
 int gogogo = 1;
 
-void ComputeSinCosTables(void)
-{
-	//This function will save the math processing time of the code.  If the user wishes to not
-	//perform this function, these tables can be generated and saved as a constant.
-	uint8_t ZoneNum;
-	for (ZoneNum = 0; ZoneNum < 64; ZoneNum++)
-	{
-		SinOfPitch[ZoneNum] = sin((VL53L5_Zone_Pitch8x8[ZoneNum])*M_PI/180);
-		CosOfPitch[ZoneNum] = cos((VL53L5_Zone_Pitch8x8[ZoneNum])*M_PI/180);
-		SinOfYaw[ZoneNum] = sin(VL53L5_Zone_Yaw8x8[ZoneNum]*M_PI/180);
-		CosOfYaw[ZoneNum] = cos(VL53L5_Zone_Yaw8x8[ZoneNum]*M_PI/180);
-	}
-}
+//https://community.st.com/t5/imaging-sensors/vl53l5cx-multi-zone-sensor-get-x-y-z-of-points-relative-to/td-p/172929
+//https://community.st.com/t5/imaging-sensors/vl53l5cx-distances-don-t-match-theory-what-am-i-missing/td-p/82657
 
 void ConvertDist2XYZCoords8x8(VL53L5CX_ResultsData *ResultsData, sensor_msgs::PointCloud* point_cloud)
 {
 	uint8_t ZoneNum;
-	float Hyp;
+    double dist_m;
 	for (ZoneNum = 0; ZoneNum < 64; ZoneNum++)
 	{
 		if ((ResultsData->nb_target_detected[ZoneNum] > 0) && (ResultsData->distance_mm[ZoneNum] > 0) && ((ResultsData->target_status[ZoneNum] == 5) || (ResultsData->target_status[ZoneNum] == 6) || (ResultsData->target_status[ZoneNum] == 9)) )
 		{
             geometry_msgs::Point32 p;
-			Hyp = ResultsData->distance_mm[ZoneNum]/SinOfPitch[ZoneNum];
-			p.y = CosOfYaw[ZoneNum]*CosOfPitch[ZoneNum]*Hyp*0.001;
-			p.z = SinOfYaw[ZoneNum]*CosOfPitch[ZoneNum]*Hyp*-0.001;
-			p.x = ResultsData->distance_mm[ZoneNum]*0.001;
+            dist_m = ResultsData->distance_mm[ZoneNum]*0.001;
+            p.y = CosOfYaw[ZoneNum]*TanOfPitch[ZoneNum]*dist_m;
+			p.z = -SinOfYaw[ZoneNum]*TanOfPitch[ZoneNum]*dist_m;
+			p.x = dist_m;
             point_cloud->points.push_back(p);
 		}
 	}
@@ -161,8 +161,6 @@ int main(int argc, char** argv)
     sigemptyset(&abort_act.sa_mask);
     abort_act.sa_flags = 0;
     sigaction(SIGINT, &abort_act, NULL);
-
-    ComputeSinCosTables();
 
     ros::init(argc, argv, "tof", ros::init_options::NoSigintHandler);
     ros::NodeHandle nh("~");
@@ -209,7 +207,7 @@ int main(int argc, char** argv)
 	/* Set resolution in 8x8. WARNING : As others settings depend to this
 	 * one, it must be the first to use.
 	 */
-	status = vl53l5cx_set_resolution(&Dev, VL53L5CX_RESOLUTION_8X8);
+	status = vl53l5cx_set_resolution(&Dev, VL53L5CX_RESOLUTION_4X4);
 	if(status)
 	{
 		printf("vl53l5cx_set_resolution failed, status %u\n", status);
@@ -220,7 +218,7 @@ int main(int argc, char** argv)
 	 * Using 4x4, min frequency is 1Hz and max is 60Hz
 	 * Using 8x8, min frequency is 1Hz and max is 15Hz
 	 */
-	status = vl53l5cx_set_ranging_frequency_hz(&Dev, 15);
+	status = vl53l5cx_set_ranging_frequency_hz(&Dev, 30);
 	if(status)
 	{
 		printf("vl53l5cx_set_ranging_frequency_hz failed, status %u\n", status);
